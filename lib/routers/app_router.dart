31373136cdd737549fd2cf7a03ce4b490f92e299
Suyash Rane <suyashrane9923@gmail.com>
2024-02-12 18:25:49 +0530

    mac update

--- lib/routers/app_router.dart
+++ lib/routers/app_router.dart
@@ -16,6 +16,7 @@ class AppRouter {
 
       bool outletcheck = databaseCubit.outletId == null;
       bool terminalTypecheck = deviceControl.mainTerminal == null;
+      print('Terminal Check $terminalTypecheck');
       if (outletcheck) {
         return AppScreen.outletDataRegisterScreen.path;
       }

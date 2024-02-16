0df2c968d316232f0b79519f3fbcf5c4bf535461
VikasAgrawal-04 <vikash.agarawal2012@gmail.com>
2024-02-16 16:16:19 +0530

    merging

--- lib/bloc/screen_cubits/user_management_cubit/screen_function_cubit/screen_function_state.dart
+++ lib/bloc/screen_cubits/user_management_cubit/screen_function_cubit/screen_function_state.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 import 'package:zimbapos/global/utils/status_handler/status_handler.dart';
 import 'package:zimbapos/models/global_models/screen_function_mapping_model.dart';
